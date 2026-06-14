4e84855aa81d308c259a0c2d63be0035220c24e1
haruyukiezz <haru23grau@gmail.com>
2026-06-13 23:20:11 -0300

    Update autojump.lua

--- autojump.lua
+++ autojump.lua
@@ -2,8 +2,4 @@ local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
 
-while true do
-    task.wait(1)
-    humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-    print("Pulando")
-end
+humanoid.WalkSpeed = 50
